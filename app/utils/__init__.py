f67e8d2466b71816ea902eff92a3f038b6a84fcf
arlette sakgouok <arlysimo@gmail.com>
2024-12-06 09:25:15 +0100

    modif general

--- app/utils/__init__.py
+++
@@ -1,7 +0,0 @@
-# app/utils/__init__.py
-
-from .scraper import get_stock_data
-from .charts_data import prepare_chart_data 
-from .metrics import get_financial_metrics
-from .news import get_apple_news
-
